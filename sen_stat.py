55a19ec532fd03a306c72574f8cb1a0be3b1a32a
sajad <sajastu94_github>
2021-10-01 03:44:54 -0400

    update general_stat.py

---
+++ sen_stat.py
@@ -0,0 +1,19 @@
+import pickle
+import numpy as np
+
+objects = {}
+with (open("myfile", "rb")) as openfile:
+    objects = pickle.load(openfile)
+
+
+src_sent_len = np.asarray(objects['src_sent_len_list'])
+tgt_sent_len = np.asarray(objects['tgt_sent_len_list'])
+print('src')
+print(np.mean(src_sent_len))
+print(np.min(src_sent_len))
+print(np.max(src_sent_len))
+
+print('tgt')
+print(np.mean(tgt_sent_len))
+print(np.min(tgt_sent_len))
+print(np.max(tgt_sent_len))
